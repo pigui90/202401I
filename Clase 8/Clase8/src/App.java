bf740dafe8ebcffc5424d5d37a2c242d6264fab7
scampos <sergio.campos.rodriguez@una.cr>
2024-03-04 19:17:39 -0600

    Clase 8 Ejemplo 1

--- Clase 8/Clase8/src/App.java
+++ Clase 8/Clase8/src/App.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Variables
+        int lado;
+        int altura;
+        int base;
+        int opcion;
+        int area;
+        boolean flag = true;
+        // Variable Scanner que sirve para leer valores desde la consola
+        Scanner scanner = new Scanner(System.in);
+        while (flag == true) {// Esto significa que lo haga mientras true == true
+            System.out
+                    .println(
+                            "Digite la figura a la que le quiere calcular el area: \n1-Cuadrado \n2-Triangulo \n3-Rectangulo \n4-Salir");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:// En caso de que sea cuadrado
+                    System.out.println("Digite la medida del lado del cuadrado");
+                    lado = scanner.nextInt();
+                    area = lado * lado;
+                    System.out.println("El area del cuadrado es " + area);
+                    break;
+                case 2:
+                    System.out.println("Digite la medida de la base del triangulo");
+                    base = scanner.nextInt();
+                    System.out.println("Digite la medida de la altura del triangulo");
+                    altura = scanner.nextInt();
+                    System.out.println("El area del triangulo es " + ((base * altura) / 2));
+                    break;
+                case 3:
+                    System.out.println("Digite la medida de la base del rectangulo");
+                    base = scanner.nextInt();
+                    System.out.println("Digite la medida de la altura del rectangulo");
+                    altura = scanner.nextInt();
+                    System.out.println("El area del rectangulo es " + (base * altura));
+                    break;
+                case 4:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }
+
+        System.out.println("Fin del programa");
+        scanner.close();
+
     }
 }
